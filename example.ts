917f60883dd8683003138745630dd79b2230acb8
Dhruv Verma <dhruvv90@gmail.com>
2021-09-10 16:28:52 +0530

    Modify Example

--- example.ts
+++ example.ts
@@ -1,30 +1,50 @@
-import { Job } from "./src/models/job";
+import { Job, JobStatus } from "./src/models/job";
 import { Scheduler } from "./src/models/scheduler";
 import { SyncTask } from "./src/models/task";
 
 
-const myFunc = ()=>{
-    console.log('Test...');
+const everySecond = () => {
+    console.log('First Task - every second...');
+}
+
+const every3Seconds = () => {
+    console.log('Second Task - every three seconds...');
 }
 
 const scheduler = new Scheduler();
-const task = new SyncTask('testTask', myFunc, (e)=> console.log(e));
-const job = new Job(task, {seconds: 2, runAtStart: true});
+
+const task = new SyncTask('firstTask', everySecond);
+const task2 = new SyncTask('secondTask', every3Seconds);
+
+const job = new Job(task, { seconds: 1, runAtStart: true });
+const job2 = new Job(task2, { seconds: 3 })
 
 scheduler.addJob(job);
+scheduler.addJob(job2);
 
-setTimeout(()=>{
+setInterval(() => {
     console.log('Scheduler status');
     console.log(scheduler.status());
-    scheduler.stop();
-}, 8000);
+}, 6000);
 
-process.on('uncaughtException',(e,o)=>{
+setTimeout(()=>{
+    job2.stop();
+}, 18000);
+
+
+setTimeout(()=>{
+    scheduler.stop();
+    process.exit()
+}, 25000)
+
+
+
+process.on('uncaughtException', (e, o) => {
     console.log(e.message);
 })
 
 
-process.on('unhandledRejection', (r,p)=>{
+process.on('unhandledRejection', (r, p) => {
     console.log(r);
 })
 
