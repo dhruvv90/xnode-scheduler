dc3c59de57b6fa92ec55e05d783074edcd5f0e3f
Dhruv Verma <dhruvv90@gmail.com>
2021-09-13 18:52:36 +0530

    Lint

--- lib/models/job.ts
+++ lib/models/job.ts
@@ -39,7 +39,7 @@ export abstract class Job {
         timerOptions: TimerOptions,
         id?: string,
         errorHandler?: Fn<Error>,
-        runAtStart: boolean = true
+        runAtStart = true
     ) {
         this.fn = fn;
         this.id = id || v4();
--- lib/models/scheduler.ts
+++ lib/models/scheduler.ts
@@ -62,7 +62,5 @@ export class Scheduler {
             activeJobs,
             idleJobs,
         }
-
     }
-
 }
